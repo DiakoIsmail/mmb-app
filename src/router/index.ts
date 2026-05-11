d3b30233fd132b03d871a1f640752de79916d5d3
Diako Ismail <diakoismail@Diakos-MacBook-Pro.local>
2026-05-11 17:25:25 +0200

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import InstagramView from '../views/InstagramView.vue'
-import VarBeratelseView from '../views/VarBeratelseView.vue'
-import OrderView from '../views/OrderView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import InstagramView from "../views/InstagramView.vue";
+import VarBeratelseView from "../views/VarBeratelseView.vue";
+import OrderView from "../views/OrderView.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomeView },
-    { path: '/instagram', component: InstagramView },
-    { path: '/var-berattelse', component: VarBeratelseView },
-    { path: '/bestall/:type', component: OrderView },
+    { path: "/", component: HomeView },
+    { path: "/mmb-app", component: HomeView },
+    { path: "/instagram", component: InstagramView },
+    { path: "/var-berattelse", component: VarBeratelseView },
+    { path: "/bestall/:type", component: OrderView },
   ],
   scrollBehavior: () => ({ top: 0 }),
-})
+});
